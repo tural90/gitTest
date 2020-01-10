035102e3f4b67ac5e31f86bed22d2d7f5c8d0110
turalfataliyev <55106827+turalfataliyev@users.noreply.github.com>
2020-01-09 19:10:16 -0500

    Update main.java

--- src/test/java/Main/main.java
+++ src/test/java/Main/main.java
@@ -3,7 +3,7 @@ package Main;
 public class main {
 
     public static void main(String[] args) {
-        //changes made to remote class
+        // made to remote class
         //changes made to remote class 1
     }
 }
