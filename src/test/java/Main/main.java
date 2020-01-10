fbcbe2096f3b93e168bfac875202774bab40f047
turalfataliyev <55106827+turalfataliyev@users.noreply.github.com>
2020-01-09 22:21:33 -0500

    Update main.java

--- src/test/java/Main/main.java
+++ src/test/java/Main/main.java
@@ -5,7 +5,7 @@ public class main {
     public static void main(String[] args) {
         // made to remote class adsadasda
         //changes made to remote class 1
-        //remote addition
+        //a addition
     }
 }
 
