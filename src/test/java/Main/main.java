28b289446cc88def81b1d96995dc47bf3c5a14ab
turalfataliyev <turalfetali@yahoo.com>
2020-01-09 22:20:08 -0500

    adsadsaqqq

--- src/test/java/Main/main.java
+++ src/test/java/Main/main.java
@@ -3,10 +3,9 @@ package Main;
 public class main {
 
     public static void main(String[] args) {
-        // made to remote class
+        // made to remote class adsadasda
         //changes made to remote class 1
         //remote addition
-            qw  qw  qw  
     }
 }
 
